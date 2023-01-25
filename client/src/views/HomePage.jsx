9441a01f3a055b9d0af634833186a609ac3b8335
Zaid Okal <zokal@uwo.ca>
2023-01-25 18:23:37 -0500

    Added button to go to login page

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./HomePage.module.css";
+import { Link } from "react-router-dom";
+import AccountButton from "../components/AccountButton";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
+      <div>
         <h1>Hello!</h1>
-    </div>
+      </div>
+
+      <div className={styles.AccountDiv1}>
+        <Link>
+          <div>
+            <AccountButton
+              className={styles.Button}
+              linkTo={"/login"}
+              text={"LOGIN"}
+            />
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
