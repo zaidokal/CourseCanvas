1ed499ad2d2fd359a472dec739efc0b7802c3766
Zaid Okal <zokal@uwo.ca>
2023-01-25 18:23:01 -0500

    Added routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./views/HomePage";
+import { Register } from "./views/Register";
+import { ChangePassword } from "./views/ChangePassword";
+
+import { Login } from "./views/Login";
 
 const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/ChangePassword" element={<ChangePassword />} />
     </Routes>
   );
 };
