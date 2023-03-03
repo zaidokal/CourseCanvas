e5e841835bfeacf7a22b64b6d604baffaa203ab4
Zaid Okal <zokal@uwo.ca>
2023-03-02 22:38:35 -0500

    Added AssignInstructor button link in homepage

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -52,6 +52,12 @@ const HomePage = () => {
               text={"COTemplate"}
             />
           </div>
+          <div className={styles.header}>
+            <AccountButton
+              text={"AssignInstructor"}
+              linkTo={"/AssignInstructor"}
+            />
+          </div>
         </Link>
       </div>
     </>
