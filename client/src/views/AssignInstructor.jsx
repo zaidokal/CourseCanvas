ddc6f6c776c27e70cfd64f9eab2468bc577a07af
Zaid Okal <zokal@uwo.ca>
2023-03-02 22:36:59 -0500

    Created AssignInstructor page

---
+++ client/src/views/AssignInstructor.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import styles from "./AssignInstructor.module.css";
+import AccountButton from "../components/AccountButton";
+import ChangeButton from "../components/ChangeButton";
+
+export const AssignInstructor = (props) => {
+  return <></>;
+};
