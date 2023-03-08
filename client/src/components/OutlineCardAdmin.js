f39ee2f6adadfc1fe5a777cf3364f9c1cd370fd1
Zaid Okal <zokal@uwo.ca>
2023-03-08 17:46:23 -0500

    Added reject function and message for approve and reject

--- client/src/components/OutlineCardAdmin.js
+++ client/src/components/OutlineCardAdmin.js
@@ -28,6 +28,78 @@ const OutlineCardAdmin = (props) => {
       .catch((err) => {
         console.log(err);
       });
+
+    axios
+      .post(
+        `http://localhost:8000/api/secure/reply/${outline._id}`,
+        {
+          decider: "Approved",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        // Call the onApprove prop function passed from the parent component
+        props.onApprove(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    window.location.reload();
+  };
+
+  const rejectCO = () => {
+    axios
+      .post(
+        `http://localhost:8000/api/secure/request/${outline._id}`,
+        {
+          requestApprove: false,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        // Call the onApprove prop function passed from the parent component
+        props.onApprove(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .post(
+        `http://localhost:8000/api/secure/reply/${outline._id}`,
+        {
+          decider: "Rejected",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        // Call the onApprove prop function passed from the parent component
+        props.onApprove(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    window.location.reload();
     window.location.reload();
   };
 
@@ -44,6 +116,9 @@ const OutlineCardAdmin = (props) => {
         </Link>
         <button className={styles.ApproveBtn} onClick={approveCO}>
           Approve
+        </button>
+        <button className={styles.ApproveBtn} onClick={rejectCO}>
+          Reject
         </button>
       </div>
     </>
