302ab2cffbaabe4fc8a7a4c26059e7be064c0178
Zaid Okal <zokal@uwo.ca>
2023-03-14 20:34:50 -0400

    Removed Head div and changed it to Header component

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -3,7 +3,7 @@ import styles from "./HomePage.module.css";
 import { Link } from "react-router-dom";
 import OutlineCard from "../components/OutlineCard";
 import axios from "axios";
-import WesternLogoMini from "../Images/WesternLogoMini.png";
+import Header from "../components/Header";
 
 const HomePage = () => {
   const [outlineList, setOutlineList] = useState({
@@ -85,36 +85,8 @@ const HomePage = () => {
 
   return (
     <>
+      <Header></Header>
       <div className={styles.MainDiv}>
-        <div className={styles.Head}>
-          <img
-            className={styles.WesternLogoMini}
-            src={WesternLogoMini}
-            alt="Western Mini Logo"
-          />
-
-          <div className={styles.OutlineManager}>Outline Manager</div>
-
-          <Link to="/AssignInstructor">
-            <button className={styles.LinkButtons}>AssignInstructor</button>
-          </Link>
-          <Link to="/AdminApproval">
-            <button className={styles.LinkButtons}>AdminApproval</button>
-          </Link>
-
-          <div className={styles.RightDiv}>
-            <div className={styles.UsernameDiv}>
-              <p>FirstName LastName</p>
-            </div>
-
-            <div className={styles.logoutDiv}>
-              <Link to="/">
-                <button className={styles.logoutBtn}>Logout</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-
         <div className={styles.container}>
           <div className={styles.title}>Current Outlines</div>
           <div className={styles.PostTitle}></div>
