1ea3e9658a627afffdbc88180ea99bb06a3799c3
Zaid Okal <zokal@uwo.ca>
2023-03-17 15:42:37 -0400

    Added drop down menu

--- client/src/views/HomePageDirector.jsx
+++ client/src/views/HomePageDirector.jsx
@@ -12,7 +12,7 @@ const HomePageDirector = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/secure/all-outlines", {
+      .get("http://localhost:8000/api/secure/all-outlines-approval", {
         headers: {
           "Content-Type": "application/json",
         },
@@ -31,6 +31,40 @@ const HomePageDirector = () => {
   const displayList = outlineList.outlines.map((out) => (
     <OutlineCard outline={out} key={out._id} />
   ));
+
+  const [courseNames, setCourseNames] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/secure/instructor/assigned-courses", {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response.data[0].assignedCourses); // log the data returned by the server
+        setCourseNames(response.data[0].assignedCourses);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const [selectedCourse, setSelectedCourse] = useState("");
+
+  const displayCourses = (
+    <select
+      className={styles.Display}
+      value={selectedCourse}
+      onChange={(e) => setSelectedCourse(e.target.value)}
+    >
+      <option>Select Course</option>
+      {courseNames.map((cou) => (
+        <option key={cou} value={cou}>
+          {cou}
+        </option>
+      ))}
+    </select>
+  );
 
   const [isHovered, setIsHovered] = useState(false);
 
@@ -58,37 +92,15 @@ const HomePageDirector = () => {
     }
   });
 
-  const containerRef2 = useRef(null);
-  const scrollIntervalRef2 = useRef(null);
-  const right2 = useRef(null);
-  const left2 = useRef(null);
-
-  const handleScrolling2 = () => {
-    const container2 = containerRef2.current;
-
-    if (isHovered === "right2") {
-      container2.scrollLeft += 70;
-    } else if (isHovered === "left2") {
-      container2.scrollLeft -= 70;
-    } else {
-      clearInterval(scrollIntervalRef2.current);
-    }
-  };
-
-  useEffect(() => {
-    if (isHovered) {
-      scrollIntervalRef2.current = setInterval(handleScrolling2, 100);
-    } else {
-      clearInterval(scrollIntervalRef2.current);
-    }
-  });
-
   return (
     <>
       <Header></Header>
+
       <div className={styles.MainDiv}>
+        {displayCourses}
+
         <div className={styles.container}>
-          <div className={styles.title}>Current Outlines</div>
+          <div className={styles.title}>All Outlines</div>
           <div className={styles.PostTitle}></div>
           <div
             className={styles.LeftScroll}
@@ -97,9 +109,6 @@ const HomePageDirector = () => {
             ref={left}
           ></div>
           <div ref={containerRef} className={styles.YOutline} id="Outlines">
-            <Link to="/COTemplate">
-              <button className={styles.COTemp}>+</button>
-            </Link>
             {displayList}
           </div>
           <div
@@ -110,27 +119,10 @@ const HomePageDirector = () => {
           ></div>
         </div>
 
-        <div className={styles.container}>
-          <div className={styles.title}>Previous Outlines</div>
-          <div className={styles.PostTitle}></div>
-          <div
-            className={styles.LeftScroll}
-            onMouseEnter={() => setIsHovered("left2")}
-            onMouseLeave={() => setIsHovered(false)}
-            ref={left2}
-          ></div>
-          <div ref={containerRef2} className={styles.YOutline} id="Outlines">
-            <Link to="/COTemplate">
-              <button className={styles.COTemp}>+</button>
-            </Link>
-            {displayList}
-          </div>
-          <div
-            className={styles.RightScroll}
-            onMouseEnter={() => setIsHovered("right2")}
-            onMouseLeave={() => setIsHovered(false)}
-            ref={right2}
-          ></div>
+        <div className={styles.ButtonDiv}>
+          <Link to="/AdminApproval">
+            <button className={styles.LinkButtons}>Admin Approval</button>
+          </Link>
         </div>
 
         <div className={styles.footer}>© Built and Designed by SRZ</div>
