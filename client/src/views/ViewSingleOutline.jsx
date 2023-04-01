293f9ac327f186733186333a549674ddfe00418b
Rakhshan Khan <mkha45@uwo.ca>
2023-04-01 01:43:23 -0400

    Fix printing and CSS issues

--- client/src/views/ViewSingleOutline.jsx
+++ client/src/views/ViewSingleOutline.jsx
@@ -181,17 +181,19 @@ const ViewSingleOutline = (props) => {
   const contentRef = useRef(null);
 
   const handleDownload = () => {
-    if (userInput.decision === "Approved") {
-      const content = contentRef.current;
-      const options = {
-        margin: 1,
-        filename: userInput.courseName,
-        image: { type: "jpeg", quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-      };
-      html2pdf().set(options).from(content).save();
-    }
+    // if (userInput.decision === "Approved") {
+    //   const content = contentRef.current;
+    //   const options = {
+    //     margin: 1,
+    //     filename: userInput.courseName,
+    //     image: { type: "jpeg", quality: 0.98 },
+    //     html2canvas: { scale: 2 },
+    //     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    //   };
+    //   html2pdf().set(options).from(content).save();
+    // }
+
+    window.print();
   };
 
   const [width, setWidth] = useState("100px");
